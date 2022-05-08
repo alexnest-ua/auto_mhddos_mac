2f16c51a858e78ebf94b93a11cc4967c4d0343c7
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-08 18:19:45 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install coreutils
 brew install git
@@ -13,8 +15,6 @@ cd ~/mhddos_proxy
 echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mInstalling latest requirements...\033[0;0m\n\n"
 sleep 2
 python3 -m pip install -r requirements.txt
-
-set -e 
 
 restart_interval="1200"
 
