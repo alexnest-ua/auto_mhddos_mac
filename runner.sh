f4d96117a2dba5f210aa6be8e8269f80ed911a99
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-08 18:32:41 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -7,9 +7,9 @@ brew install coreutils git python@3.10
 python3.10 -m pip install --upgrade setuptools
 
 cd ~
-rm -rf auto_mhddos_mac || true
+rm -rf auto_mhddos_mac
 git clone https://github.com/alexnest-ua/auto_mhddos_mac || true 
-rm -rf mhddos_proxy || true
+rm -rf mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy || true 
 cd ~/mhddos_proxy
 echo -e "\n\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mInstalling latest requirements...\033[0;0m\n\n"
