d0a29a6663758fc56c3754b32c37a9f149412b00
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-08 18:05:45 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -81,7 +81,7 @@ do
 		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running up to date mhddos_proxy"
 	else
 		cd ~/mhddos_proxy
-		pip3 install -r requirements.txt
+		python3 -m pip install -r requirements.txt
 		clear
 		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated mhddos_proxy"
 		sleep 3
