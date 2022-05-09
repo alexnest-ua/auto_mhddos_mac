1457fcd0418075edae52d0e9634a68f9831e810e
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-09 13:14:34 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -110,8 +110,7 @@ do
 	else
 		clear
 		echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running updated auto_mhddos_alexnest"
-		bash runner.sh $num_of_copies $threads $rpc $debug $vpn& # run new downloaded script 
-		exit
+		bash runner.sh $num_of_copies $threads $rpc $debug $vpn # run new downloaded script 
 	fi
    	
 	sleep 3
