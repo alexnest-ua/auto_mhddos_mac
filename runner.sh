3ed57808910b2ec66fe06da0cdbfd1a4d4acec28
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-10 15:46:57 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -62,7 +62,7 @@ then
 fi
 
 debug="${3:-}"
-if [ "${debug}" != "--debug" ] && [ "${debug}" != "" ];
+if [ "${debug}" != "--debug" ] && [ "${debug}" != "" ] && [ "${debug}" != "--vpn" ];
 then
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mStarting with parameter --debug (--table is not supported in our script)\033[0;0m\n"
 	debug="--debug"
