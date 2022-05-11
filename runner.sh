66829fcfd99740bc92648852b11a1f4ef2f2169c
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-11 13:49:44 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -87,7 +87,7 @@ fi
 echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;32mStarting attack with such parameters:  -t $threads --rpc $rpc $debug $vpn...\033[1;0m"
 sleep 7
 
-trap 'echo signal received!; kill "${PID}"; wait "${PID}"; kill "${PID1}"; wait "${PID1}"' SIGINT SIGTERM
+trap 'echo signal received!; kill "${PID}"; kill "${PID1}";  wait "${PID}"; wait "${PID1}"' SIGINT SIGTERM
 
 # Restarts attacks and update targets list every 20 minutes
 while [ 1 == 1 ]
